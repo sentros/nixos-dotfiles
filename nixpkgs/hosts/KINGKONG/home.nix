9246f0ebfc36383976a886da5909fca4fa170096
sentros <julius.stenros@gmail.com>
2025-07-02 14:44:48 +0300

    nix: Handle ghostty dotfiles purely via home manager

--- nixpkgs/hosts/KINGKONG/home.nix
+++ nixpkgs/hosts/KINGKONG/home.nix
@@ -203,9 +203,37 @@
       clipboard-read = "allow";
       clipboard-write = "allow";
       window-theme = "dark";
-      theme = "Monokai Pro";
+      theme = "monokai-pro";
       font-size = 12;
       font-family = "Hack";
+    };
+    themes = {
+      monokai-pro = {
+        background = "2d2a2e";
+        cursor-color = "c1c0c0";
+        cursor-text = "c1c0c0";
+        foreground = "fcfcfa";
+        palette = [
+          "0=#2d2a2e"
+          "1=#ff6188"
+          "2=#a9dc76"
+          "3=#ffd866"
+          "4=#fc9867"
+          "5=#ab9df2"
+          "6=#78dce8"
+          "7=#fcfcfa"
+          "8=#727072"
+          "9=#ff6188"
+          "10=#a9dc76"
+          "11=#ffd866"
+          "12=#fc9867"
+          "13=#ab9df2"
+          "14=#78dce8"
+          "15=#fcfcfa"
+        ];
+        selection-background = "5b595c";
+        selection-foreground = "fcfcfa";
+      };
     };
   };
 
