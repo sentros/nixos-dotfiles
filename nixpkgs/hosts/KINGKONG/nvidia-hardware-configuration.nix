85fb53093e784f98009ebffa4c1374a518c65dc8
sentros <julius.stenros@gmail.com>
2025-07-11 14:49:40 +0300

    nix: Pin nvidia driver version to latest one on nvidia website

--- nixpkgs/hosts/KINGKONG/nvidia-hardware-configuration.nix
+++ nixpkgs/hosts/KINGKONG/nvidia-hardware-configuration.nix
@@ -18,7 +18,15 @@
     nvidiaSettings = true;
 
     # Driver version
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.169";
+      sha256_64bit = "sha256-XzKoR3lcxcP5gPeRiausBw2RSB1702AcAsKCndOHN2U=";
+      sha256_aarch64 = "sha256-s8jqaZPcMYo18N2RDu8zwMThxPShxz/BL+cUsJnszts=";
+      openSha256 = "sha256-oqY/O5fda+CVCXGVW2bX7LOa8jHJOQPO6mZ/EyleWCU=";
+      settingsSha256 = "sha256-0E3UnpMukGMWcX8td6dqmpakaVbj4OhhKXgmqz77XZc=";
+      persistencedSha256 = "sha256-dttFu+TmbFI+mt1MbbmJcUnc1KIJ20eHZDR7YzfWmgE=";
+    };
   };
 
   # Load nvidia driver for Xorg and Wayland
