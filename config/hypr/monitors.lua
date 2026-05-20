2fd8fff4d3f545543a0edc82ebe36d9ed9eaf08b
Julius Stenros <julius.stenros@veikkaus.fi>
2026-05-20 08:46:49 +0300

    Fix hyprland monitor position

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -5,18 +5,21 @@ hl.monitor({
   output = "HDMI-A-1",
   mode = "3840x2160@119.88",
   scale = 1.6,
-  vrr = 3
+  vrr = 3,
+  position = "1800x0",
 })
 
 hl.monitor({
   output = "eDP-1",
   mode = "2880x1800@120.00",
   scale = 1.6,
-  vrr = 1
+  vrr = 1,
+  position = "0x0",
 })
 
 hl.monitor({
   output = "DP-1",
   mode = "3440x1440@99.98",
   scale = 1.25,
+  position = "1800x0",
 })
